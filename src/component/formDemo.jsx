4eb87db91e7411f5c26a995cc0fdf5fcf28e1253
e <zenght@jpush.cn>
2018-10-16 16:44:05 +0800

    feature:form switch

--- src/component/formDemo.jsx
+++ src/component/formDemo.jsx
@@ -45,13 +45,61 @@ export default class FormDemo extends Component {
                     </div>
                 </div>
                 <div className="m32">
-                    <div class="ptb16 plr6 d-il ta-c table-title s0">
-                        <div class="checkbox-box-normalize">
-                            <input id="checkbox_normalize_title" type="checkbox" name="c_n"/>
-                            <span class="checkbox-hook ta-c">
-                                <span class="checkbox-hook-in fs12 op0">✓</span>
+                    <div className="ptb16 plr6 d-il ta-c table-title s0">
+                        <div className="checkbox-box-normalize">
+                            <input id="checkbox_normalize_title" type="checkbox" name="c_n" defaultChecked/>
+                            <span className="checkbox-hook ta-c">
+                                <span className="checkbox-hook-in fs12 op0">✓</span>
                             </span>
-                            <label for="checkbox_normalize_title" class="p-r z10"></label>
+                            <label htmlFor="checkbox_normalize_title" className="p-r z10"></label>
+                        </div>
+                    </div>
+                    <div className="ptb16 plr6 d-il ta-c table-title s0">
+                        <div className="checkbox-box-normalize">
+                            <input id="checkbox_normalize_title_1" type="checkbox" name="c_n"/>
+                            <span className="checkbox-hook ta-c">
+                                <span className="checkbox-hook-in fs12 op0">✓</span>
+                            </span>
+                            <label htmlFor="checkbox_normalize_title_1" className="p-r z10"></label>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="radio-box-normalize d-il pr16">
+                        <input
+                            id="isCallbackReport1"
+                            type="radio"
+                            name="managerCallbackReport"
+                            defaultChecked
+                            className="d-n"/>
+                        <label className="m0" htmlFor="isCallbackReport1">
+                            <div className="radio-out">
+                                <i className="radio-in"></i>
+                            </div>
+                            <span>Yes</span>
+                        </label>
+                    </div>
+                    <div className="radio-box-normalize d-il pr16">
+                        <input
+                            id="isCallbackReport2"
+                            type="radio"
+                            name="managerCallbackReport"
+                            className="d-n"/>
+                        <label className="m0" htmlFor="isCallbackReport2">
+                            <div className="radio-out">
+                                <i className="radio-in"></i>
+                            </div>
+                            <span>No</span>
+                        </label>
+                    </div>
+                    <div className="m32">
+                        <div className="switch-box-normalize">
+                            <label htmlFor="switch-input" className="switch-mark-click"></label>
+                            <input type="checkbox" defaultValue="1" id="switch-input"/>
+                            <div>
+                                <i className="switch-move"></i>
+                                <span className="switch-horizon"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
