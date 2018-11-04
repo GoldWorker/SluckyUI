999ee6840c3661c62ea885760db7091d48eca313
Flyteng <695508580@qq.com>
2018-11-04 16:24:22 +0800

    color

--- src/component/formDemo.jsx
+++ src/component/formDemo.jsx
@@ -1,30 +1,42 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class FormDemo extends Component {
     render() {
         return (
             <div>
+                <input type="text" name="user_name" id="user_name" className="input input-normal" maxLength="15" placeholder="输入你的姓名" autoFocus required />
+                <div className="search-box-normalize">
+                    <input type="text" className="input input-normal" />
+                    <button className="btn ptb6 mr2 bg-blue c-text-w">搜索</button>
+                </div>
+                <div className="fileup-container">
+                    <div className="fileup">
+                        <label htmlFor="input-file" className="fileup-in btn ptb4 plr16 c-text-w bg-theme">点击选择</label>
+                        <input type="file" id="input-file" />
+                    </div>
+                    <img src="" alt="" />
+                </div>
                 <div className="m32">
                     <div className="input ml24 input-search">
                         <label htmlFor="" className="fs12 c-hint-b mb0">Name | Email</label>
-                        <br/>
+                        <br />
                         <input
                             type="text"
                             className="input-down"
                             placeholder="What's you name?"
-                            maxLength="50"/>
+                            maxLength="50" />
                     </div>
                 </div>
 
                 <div className="m32">
                     <div className="input ml24 input-search">
                         <label htmlFor="" className="fs12 c-hint-b mb0">Name | Email</label>
-                        <br/>
+                        <br />
                         <input
                             type="text"
                             className="input-down"
                             placeholder="What's you name?"
-                            maxLength="50"/>
+                            maxLength="50" />
                         <ul className="input-option paper bor b-side fs12 p0" scrollbar='normal'>
                             <li className="p4 bor-b b-side d-f jc-b">
                                 123asdf
@@ -47,7 +59,7 @@ export default class FormDemo extends Component {
                 <div className="m32">
                     <div className="ptb16 plr6 d-il ta-c table-title s0">
                         <div className="checkbox-box-normalize">
-                            <input id="checkbox_normalize_title" type="checkbox" name="c_n" defaultChecked/>
+                            <input id="checkbox_normalize_title" type="checkbox" name="c_n" defaultChecked />
                             <span className="checkbox-hook ta-c">
                                 <span className="checkbox-hook-in fs12 op0">✓</span>
                             </span>
@@ -56,7 +68,7 @@ export default class FormDemo extends Component {
                     </div>
                     <div className="ptb16 plr6 d-il ta-c table-title s0">
                         <div className="checkbox-box-normalize">
-                            <input id="checkbox_normalize_title_1" type="checkbox" name="c_n"/>
+                            <input id="checkbox_normalize_title_1" type="checkbox" name="c_n" />
                             <span className="checkbox-hook ta-c">
                                 <span className="checkbox-hook-in fs12 op0">✓</span>
                             </span>
@@ -71,7 +83,7 @@ export default class FormDemo extends Component {
                             type="radio"
                             name="managerCallbackReport"
                             defaultChecked
-                            className="d-n"/>
+                            className="d-n" />
                         <label className="m0" htmlFor="isCallbackReport1">
                             <div className="radio-out">
                                 <i className="radio-in"></i>
@@ -84,7 +96,7 @@ export default class FormDemo extends Component {
                             id="isCallbackReport2"
                             type="radio"
                             name="managerCallbackReport"
-                            className="d-n"/>
+                            className="d-n" />
                         <label className="m0" htmlFor="isCallbackReport2">
                             <div className="radio-out">
                                 <i className="radio-in"></i>
@@ -95,7 +107,7 @@ export default class FormDemo extends Component {
                     <div className="m32">
                         <div className="switch-box-normalize">
                             <label htmlFor="switch-input" className="switch-mark-click"></label>
-                            <input type="checkbox" defaultValue="1" id="switch-input"/>
+                            <input type="checkbox" defaultValue="1" id="switch-input" />
                             <div>
                                 <i className="switch-move"></i>
                                 <span className="switch-horizon"></span>
@@ -103,6 +115,7 @@ export default class FormDemo extends Component {
                         </div>
                     </div>
                 </div>
+                <textarea name="" id="" cols="50" rows="6" className="textarea"></textarea>
             </div>
         )
     }
