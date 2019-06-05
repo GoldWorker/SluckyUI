36a6a0b3d4caa7211873fdef40514bb3a744692a
zenght <zenght>
2019-06-05 22:00:19 +0800

    fix

--- src/slucky/slidebar.jsx
+++ src/slucky/slidebar.jsx
@@ -1,51 +1,58 @@
-import React, {Component} from 'react'
-
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 export default class Slidebar extends Component {
     render() {
         return (
             <div>
                 <div className="slidebar-box-normalize slidebar-hover-b">
-                    <input type="checkbox" id="slidebar-switch" className="d-n" defaultChecked/>
-                    <div className="slidebar bg-b">
+                    <input type="checkbox" id="slidebar-switch" className="d-n" />
+                    <div className="slidebar bg-b w256">
                         <label htmlFor="slidebar-switch" className="slidebar-tool p16 d-f ac jc-r">
                             <span className="mr16">返回</span>
                             <svg className="icon icon16" id="slidebar_in">
-                                <use xlinkHref="#icon-test"></use>
+                                <use xlinkHref="#icon-menu"></use>
                             </svg>
                             <div id="slidebar_out">
-                                {/* <svg className="icon icon24" id="slidebar_out">
-                                    <use xlinkHref="#icon-test"></use>
-                                </svg> */}
-                                <div className="d-il p8 plr16 m8 shadow bg-b c-text-w">
-                                    {/* 菜单 */}
-                                </div>
+                                <svg className="icon icon24 c-theme" id="slidebar_out">
+                                    <use xlinkHref="#icon-menu"></use>
+                                </svg>
                             </div>
                         </label>
-                        <details className="slide-down">
-                            <summary className="pl16 c-text-w ptb16">菜单列表导航栏</summary>
-                            <ul className="menu">
-                                <li>
-                                    <a>发布土地</a>
-                                </li>
-                                <li>
-                                    <a>发布种子方案</a>
-                                </li>
-                                <li>
-                                    <a>发布服务包</a>
-                                </li>
-                            </ul>
-                        </details>
-                        <ul className="menu">
+                        <ul className="menu w-full">
                             <li>
-                                <a>按钮</a>
+                                <NavLink exact to="/" className="nav-item" activeClassName="nav-item-active">首页</NavLink>
                             </li>
                             <li>
-                                <a>弹窗</a>
+                                <NavLink to="/articleList" className="nav-item" activeClassName="nav-item-active">文章</NavLink>
                             </li>
                             <li>
-                                <a>表单</a>
+                                <NavLink to="/register" className="nav-item" activeClassName="nav-item-active">成为Sluckyer</NavLink>
                             </li>
                         </ul>
+
+                        {/* <details className="slide-down">
+                            <summary className="pl16 c-text-w ptb16">菜单列表导航栏</summary>
+                            <ul className="menu w-full">
+
+                            </ul>
+                        </details>
+                        <details className="slide-down">
+                            <summary className="pl16 c-text-w ptb16">表格</summary>
+                            <ul className="menu w-full">
+                                <li>
+                                    <a className="c-placeholder" href="" >123</a>
+                                </li>
+                                <li>
+                                    <a className="c-placeholder" href="" >123</a>
+                                </li>
+                                <li>
+                                    <a className="c-placeholder" href="" >123</a>
+                                </li>
+                                <li>
+                                    <a className="c-placeholder" href="" >123</a>
+                                </li>
+                            </ul>
+                        </details> */}
                     </div>
                     <div className="mark-b"></div>
                 </div>
