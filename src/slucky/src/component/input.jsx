afdcdaa54a490fe80fa26043a01934d9945fdfc5
zenght <zenght>
2019-09-02 13:51:23 +0800

    input number component

--- src/slucky/src/component/input.jsx
+++ src/slucky/src/component/input.jsx
@@ -33,7 +33,7 @@ export class Input extends Component {
                                 id={this.props.id || this.id || ''}
                                 onChange={(e) => this.props.onChange && this.props.onChange(e.target.value)}
                                 style={{ width: this.props.width || 384 + 'px' }}
-                                value={this.props.value || ''}
+                                value={this.props.value}
                                 placeholder={this.props.placeholder || ''}
                                 type={this.props.type || 'text'}
                                 disabled={this.props.disabled || false}
@@ -52,14 +52,50 @@ export class Input extends Component {
 }
 
 class InputNumber extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: this.props.step || 1,
+            value: this.props.defaultValue || 0,
+            max: this.props.max || Infinity,
+            min: this.props.min || 0
+        };
+    }
+
+    handleChange(value) {
+        this.setState({
+            value
+        }, this.props.onChange && this.props.onChange(value));
+    }
+
+    handleClickCtrl(type) {
+        console.log(type);
+        const v = new Number(this.state.value);
+        switch (type) {
+            case 'UP':
+                if (v < this.state.max) {
+                    this.setState({
+                        value: v + this.state.step
+                    }, this.props.onChange && this.props.onChange(this.state.value));
+                }
+                break;
+            case 'DOWM':
+                if (v > this.state.min) {
+                    this.setState({
+                        value: v - this.state.step
+                    }, this.props.onChange && this.props.onChange(this.state.value));
+                }
+                break;
+        }
+    }
 
     render() {
         return (
-            <div className="d-f">
-                <Input onChange={this.props.onChange} />
+            <div className="d-f input-number">
+                <Input type='number' onChange={(v) => this.handleChange(v)} value={this.state.value} width={this.props.width || 128 + 'px'} />
                 <div className="d-il input-ctrl">
-                    <div className="ctrl-up"></div>
-                    <div className="ctrl-down"></div>
+                    <div className="ctrl-up" onClick={() => this.handleClickCtrl('UP')}></div>
+                    <div className="ctrl-down" onClick={() => this.handleClickCtrl('DOWM')}></div>
                 </div>
             </div>
         );
