f9c69fa7b128edfdb00270e3cf709dd7ac2074c4
zenght <zenght>
2019-11-13 15:05:51 +0800

    add Text component & fix Tranfer Component

--- src/slucky/src/component/text.jsx
+++ src/slucky/src/component/text.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Text extends Component {
+export class Text extends Component {
     render() {
         return (
             <div>
